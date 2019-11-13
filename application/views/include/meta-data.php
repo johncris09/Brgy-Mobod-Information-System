970db2fbd38338a016e5f9868d93183b5f6dc390
johncris09 <johncrismanabo9@gmail.com>
2019-11-13 20:33:37 +0800

    page info

---
+++ application/views/include/meta-data.php
@@ -0,0 +1,6 @@
+	<!-- Required meta tags-->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() . 'icon.ico' ?>" />
+	<!-- Title Page-->
+	<title><?php echo $page_title; ?></title>
