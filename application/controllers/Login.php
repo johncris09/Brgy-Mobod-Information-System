dae7a859cc7c85a26c098cdf2d1dea807381da0c
johncris09 <johncrismanabo9@gmail.com>
2019-11-13 20:30:40 +0800

    redirect to login page

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,8 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
  
-
 class Login extends CI_Controller
 {
     
@@ -13,9 +11,8 @@ class Login extends CI_Controller
 
   public function index()
   {
-		 
-		
-  }
-
+		$data["page_title"] = "Login";
+		$this->load->view('login', $data); 
+	} 
 }
   
