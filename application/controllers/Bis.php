951d9be535a677adca6ef3c00d0cff246c9ca94f
johncris09 <johncrismanabo9@gmail.com>
2019-11-13 20:51:48 +0800

    redirect to login if user is unauthorized

--- application/controllers/Bis.php
+++ application/controllers/Bis.php
@@ -6,7 +6,11 @@ class Bis extends CI_Controller
 { 
   public function __construct()
   {
-    parent::__construct(); 
+    parent::__construct();
+		// redirect to login if user is unauthorized
+		if (!$this->session->userdata('user_id')) {
+			redirect('login');
+		}
   }
 
   public function index()
