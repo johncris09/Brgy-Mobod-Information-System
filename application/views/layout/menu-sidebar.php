8ca4c98317a9629758512d902284c92c47e5fb26
johncris09 <johncrismanabo9@gmail.com>
2019-11-13 20:32:38 +0800

    layout

---
+++ application/views/layout/menu-sidebar.php
@@ -0,0 +1,20 @@
+		<!-- MENU SIDEBAR-->
+		<aside class="menu-sidebar d-none d-lg-block">
+			<div class="logo">
+				<a href="#">
+					<img src="<?php echo base_url() ?>assets/images/icon/logo-blue.png" alt="CoolAdmin" />
+				</a>
+			</div>
+			<div class="menu-sidebar__content js-scrollbar1">
+				<nav class="navbar-sidebar">
+					<ul class="list-unstyled navbar__list">
+						<li>
+							<a href="<?php echo base_url(); ?>">
+								<i class="fas fa-tachometer-alt"></i>Dashboard
+							</a> 
+						</li> 
+					</ul>
+				</nav>
+			</div>
+		</aside>
+		<!-- END MENU SIDEBAR-->
