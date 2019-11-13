a9764a66be6509526fa275a6a2481f0635914624
johncris09 <johncrismanabo9@gmail.com>
2019-11-13 20:51:31 +0800

    redirect to dashboard

--- application/controllers/Bis.php
+++ application/controllers/Bis.php
@@ -3,15 +3,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
  
 
 class Bis extends CI_Controller
-{
-    
+{ 
   public function __construct()
   {
-    parent::__construct();
+    parent::__construct(); 
   }
 
   public function index()
   { 
+		
+		$data["page_title"] = "Dashboard";
+		$this->load->view('index', $data);
 		
   }
 
