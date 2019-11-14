615ee029eb809d6f0a6e44b2a41bc5d9e1728f37
johncris09 <johncrismanabo9@gmail.com>
2019-11-14 19:03:51 +0800

    init

---
+++ application/controllers/Purok.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+ 
+
+class Purok extends CI_Controller
+{
+    
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function index()
+  {
+    // 
+  }
+
+}
+
+ 
