33a915517e42437172346dde65b0e21a2161b889
johncris09 <johncrismanabo9@gmail.com>
2019-11-14 11:12:24 +0800

    login function

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -8,9 +8,22 @@ class Login_model extends CI_Model {
     parent::__construct();
   }
  
-  public function index()
-  {
-    // 
-  } 
+  public function login($user_info)
+  { 
+		$this->db->where($user_info);
+		$query =  $this->db->get('user');  
+		
+		// check if user is exist
+		if($query->num_rows() > 0)
+		{ 
+			 return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+		
+	}  
+ 
 
 } 
