dc0a0c0ed0f149041de4ab5e312beb78f5e89da8
johncris09 <johncrismanabo9@gmail.com>
2019-11-14 18:39:22 +0800

    logout function

--- application/controllers/Bis.php
+++ application/controllers/Bis.php
@@ -14,11 +14,26 @@ class Bis extends CI_Controller
   }
 
   public function index()
-  { 
-		
+  {  
 		$data["page_title"] = "Dashboard";
-		$this->load->view('index', $data);
-		
-  }
+		$this->load->view('index', $data); 
+	}
 
+	public function logout()
+  {    
+		$user_id = $this->session->userdata('user_id');
+		$update_logout_date = $this->login_model->update_logout_date($user_id); 
+		if($update_logout_date)
+		{	
+			$all_sessions = $this->session->all_userdata();
+			
+			// unset all sessions
+			foreach($all_sessions as $key => $val)
+			{
+				$this->session->unset_userdata($key); 	
+			}
+
+			redirect('login');
+		}
+	} 
 } 
