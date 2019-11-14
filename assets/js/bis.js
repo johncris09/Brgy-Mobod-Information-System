8eb9e839250b0f3d78273e5ef412c7219b91aa7e
johncris09 <johncrismanabo9@gmail.com>
2019-11-14 18:20:14 +0800

    login js

--- assets/js/bis.js
+++ assets/js/bis.js
@@ -1,2 +1,21 @@
 (function ($) {  
+
+	// Login 
+	$('#login-form').on('submit', function(e){
+		e.preventDefault();    
+		$.ajax({
+			url: "login/login",
+			method: "POST",
+			data: $(this).serialize(),
+			dataType: "json",  
+			success: function(data){    
+				console.info(data) 
+			},
+			error: function(xhr,status,error){ 
+				console.info(xhr.responseText);
+			}
+		});
+	});
+	
+
 })(jQuery);
