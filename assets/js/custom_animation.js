d0592bcd4f250ac39186a7052ec2fbeeee92c201
johncris09 <johncrismanabo9@gmail.com>
2019-11-15 14:20:32 +0800

    custom animation

--- assets/js/custom_animation.js
+++ assets/js/custom_animation.js
@@ -1,3 +1,8 @@
 (function ($) {  
- 
+	$(document).on("mouseover", "td>a", function(){
+		$(this).addClass("animated jello");
+	});
+	$(document).on("mouseleave", "td>a", function(){
+		$(this).removeClass("animated jello");
+	});
 })(jQuery);
