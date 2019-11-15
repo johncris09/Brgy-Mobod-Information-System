908f258a74487816f193cb4f2153cab46b21b87f
johncris09 <johncrismanabo9@gmail.com>
2019-11-15 13:20:31 +0800

    add purok

--- assets/js/bis.js
+++ assets/js/bis.js
@@ -45,6 +45,21 @@
 
 	load_purok();
 
-	 
+	
+	$('#add_purok_form').on('submit', function (event) {
+		event.preventDefault(); 
+		$.ajax({
+			url     : "insert_purok",
+			method  : "POST",
+			data    : $(this).serialize(),
+			dataType: "json",
+			success : function (data) {
+				console.info(data)
+			},
+			error: function (xhr, status, error) { 
+				// console.info(xhr.responseText); 
+			},
+		});
+	});
   
 })(jQuery);
