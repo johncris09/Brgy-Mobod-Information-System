aeaf1a717068bc4dc26b902168afcf15e4d68da8
johncris09 <johncrismanabo9@gmail.com>
2019-11-15 18:34:01 +0800

    view user page

---
+++ application/views/admin/user.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include __DIR__ . ("\..\\include\meta-data.php"); ?>
+	<?php include __DIR__ . ("\..\\include\css-link.php"); ?>
+</head>
+
+<body class="animsition">
+	<div class="page-wrapper">
+		<?php include __DIR__ . ("\..\\layout\header-mobile.php"); ?>
+		<?php include __DIR__ . ("\..\\layout\menu-sidebar.php"); ?>
+		<!-- PAGE CONTAINER-->
+		<div class="page-container">
+			<?php include __DIR__ . ("\..\\layout\header-desktop.php"); ?>
+			<!-- MAIN CONTENT-->
+			<div class="main-content">
+				<div class="section__content section__content--p30">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-md-12 ">
+								<div class="card card-cascade narrower">
+									<div class="view view-cascade gradient-card-header purple-gradient">
+										<h2 class="card-header-title my-2 mx-2 float-left text-white"><?php echo $page_title; ?></h2>
+										<a href="<?php echo base_url() . 'user/add_user' ?>"
+											class="float-right text-white btn-rounded  btn-lg peach-gradient my-2 mx-2  " title="Add User">
+											<i class="fas fa-plus"></i> Add User
+										</a>
+									</div>
+									<div class="card-body card-body-cascade text-center">
+										<div id="user_list"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php include __DIR__ . ("\..\\layout\\footer.php"); ?>
+				</div>
+
+			</div>
+			<!-- END MAIN CONTENT-->
+			<!-- END PAGE CONTAINER-->
+		</div>
+	</div>
+	<?php include __DIR__ . ("\..\\include\js-src.php"); ?>
+</body>
+
+</html>
+<!-- end document-->
