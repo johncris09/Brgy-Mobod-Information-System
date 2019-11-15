204ab861a9dca1644f8b1ba9ebff72b2f0f4f251
johncris09 <johncrismanabo9@gmail.com>
2019-11-15 15:35:35 +0800

    delete position

--- application/models/Position_model.php
+++ application/models/Position_model.php
@@ -33,7 +33,13 @@ class Position_model extends CI_Model {
 		$query = $this->db->update('position', $position_info);
 		return $position_info['position_id'];
 	} 
- 
+
+	public function delete_position($position_info)
+	{
+		$this->db->where($position_info);
+		$query = $this->db->delete('position');
+		return $this->db->affected_rows();
+	}
 
 	
 
