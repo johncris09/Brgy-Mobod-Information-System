3562c53b961650aadc8f70279659e05e4bd653c9
johncris09 <johncrismanabo9@gmail.com>
2019-11-16 17:22:27 +0800

    add menu "household"

--- application/views/layout/header-mobile.php
+++ application/views/layout/header-mobile.php
@@ -70,6 +70,18 @@
 										</li>
 									</ul>
 								</li>
+								<li class="has-sub">
+									<a class="js-arrow open" href="#">
+										<i class="fas fa-home"></i>Household</a>
+									<ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+										<li>
+										<a href="<?php echo base_url() ?>household/add_household"> <i class="fas fa-circle-notch"></i> Add Household</a>
+										</li> 
+										<li>
+											<a href="<?php echo base_url() ?>household"><i class="fas fa-circle-notch"></i>View Household</a>
+										</li>
+									</ul>
+								</li>
 							</ul>
 						</div>
 					</nav>
--- application/views/layout/menu-sidebar.php
+++ application/views/layout/menu-sidebar.php
@@ -60,6 +60,18 @@
 									<a href="<?php echo base_url() ?>user"><i class="fas fa-circle-notch"></i>View User</a>
 								</li>
 							</ul>
+						</li> 
+						<li class="has-sub">
+							<a class="js-arrow open" href="#">
+								<i class="fas fa-home"></i>Household</a>
+							<ul class="list-unstyled navbar__sub-list js-sub-list" >
+								<li>
+								<a href="<?php echo base_url() ?>household/add_household"> <i class="fas fa-circle-notch"></i> Add Household</a>
+								</li> 
+								<li>
+									<a href="<?php echo base_url() ?>household"><i class="fas fa-circle-notch"></i>View Household</a>
+								</li>
+							</ul>
 						</li>
 					</ul>
 				</nav>
