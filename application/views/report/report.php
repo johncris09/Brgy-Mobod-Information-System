f69a473deabc9b9c6c5c59de1cb5d4f0c674f70f
johncris09 <johncrismanabo9@gmail.com>
2019-11-16 16:11:05 +0800

    report template

---
+++ application/views/report/report.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<link href="<?php echo base_url() ?>assets/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+ 
+</head>
+
+<body>
+	<div class="container">
+		<div class="text-center">
+			<h1>Brgy. Mobod</h1>
+			<p>Oroquieta City</p>
+		</div>
+		<h3><?php echo $print_title; ?></h3>
+		<?php echo $print; ?>
+
+	</div>
+</body>
+
+</html>
