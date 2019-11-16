cc98456ebedba388d702dc621d804012dfb41522
johncris09 <johncrismanabo9@gmail.com>
2019-11-16 16:41:22 +0800

    init

---
+++ application/models/Household_model.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+
+class Household_model extends CI_Model {
+ 
+  public function __construct()
+  {
+    parent::__construct();
+  }
+ 
+  public function index()
+  {
+    // 
+  }
+ 
+
+}
+  
