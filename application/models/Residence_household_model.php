aaf77c82d50f318b1b5ed63ce582c8136cc8a38b
johncris09 <johncrismanabo9@gmail.com>
2019-11-17 10:05:35 +0800

    insert residence household

--- application/models/Residence_household_model.php
+++ application/models/Residence_household_model.php
@@ -5,12 +5,15 @@ class Residence_household_model extends CI_Model {
   public function __construct()
   {
     parent::__construct();
-  }
- 
-  public function index()
-  {
-    // 
-  }
+	}
+	
+	
+	public function insert_residence_household($residence_household_info)
+	{
+		$this->db->insert('residence_household', $residence_household_info);
+		return $this->db->affected_rows();
+	}
+	
  
 
 } 
