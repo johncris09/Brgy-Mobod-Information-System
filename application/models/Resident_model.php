cbe160ab178733bef3b967638f691853e1a06bfe
johncris09 <johncrismanabo9@gmail.com>
2019-11-17 20:46:02 +0800

    get resident

--- application/models/Resident_model.php
+++ application/models/Resident_model.php
@@ -17,8 +17,18 @@ class Resident_model extends CI_Model {
 	public function insert_resident($resident_info)
 	{
 		$this->db->insert('resident', $resident_info);
-		return $this->db->insert_id(); 
-		
+		return $this->db->insert_id();  
 	} 
+	  
+	public function get_resident($resident_id)
+	{ 
+		$this->db->from('residence_household');
+		$this->db->join('household', 'household.household_id = residence_household.household_id');
+		$this->db->join('resident', 'resident.resident_id = residence_household.resident_id'); 
+		$this->db->where('resident.resident_id ', $resident_id );
+		return $this->db->get()->result_array()[0];
+	} 
+	
+
  
 }
