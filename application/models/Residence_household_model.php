091575d810199dc829caf47d7876c6fa2b313957
johncris09 <johncrismanabo9@gmail.com>
2019-11-17 19:09:52 +0800

    get resident household

--- application/models/Residence_household_model.php
+++ application/models/Residence_household_model.php
@@ -5,8 +5,16 @@ class Residence_household_model extends CI_Model {
   public function __construct()
   {
     parent::__construct();
-	}
-	
+	} 
+
+	public function get_residence_household($household_id)
+	{ 
+		$this->db->from('residence_household');
+		$this->db->join('household', 'household.household_id = residence_household.household_id');
+		$this->db->join('resident', 'resident.resident_id = residence_household.resident_id'); 
+		$this->db->where('residence_household.household_id ', $household_id );
+		return $this->db->get();
+	} 
 	
 	public function insert_residence_household($residence_household_info)
 	{
